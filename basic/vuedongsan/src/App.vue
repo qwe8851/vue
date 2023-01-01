d50ebabc0819a2ea6155a35e6a573710cfdafabb
qwe8851 <dgh07027@naver.com>
2023-01-01 22:52:45 +0900

    Vue 이벤트 핸들러로 click감지하기 (허위매물 신고버튼 추가)

--- basic/vuedongsan/src/App.vue
+++ basic/vuedongsan/src/App.vue
@@ -5,15 +5,17 @@
   <div>
     <h4>{{products[0]}}</h4>
     <p>50만원</p>    <!-- 함수를 선언할때는 ()를 빼고 -->
-    <button @click="increase">허위매물신고</button><span>신고수 : {{신고수}}</span>
+    <button @click="신고수[0]++">허위매물신고</button><span>신고수 : {{신고수[0]}}</span>
   </div>
   <div>
     <h4>{{products[1]}}</h4>
     <p>50만원</p>
+    <button @click="신고수[1]++">허위매물신고</button><span>신고수 : {{신고수[1]}}</span>
   </div>
   <div>
     <h4>{{products[2]}}</h4>
     <p>50만원</p>
+    <button @click="신고수[2]++">허위매물신고</button><span>신고수 : {{신고수[2]}}</span>
   </div>
 </template>
 
@@ -25,7 +27,7 @@ export default {
       menus : ['Home', 'Shop', 'About'],
       products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
       price : [ 50, '가격은 아무거나'],
-      신고수 : 0
+      신고수 : [0,0,0]
     }
   },
   methods: {    // 함수 만드는 공간
