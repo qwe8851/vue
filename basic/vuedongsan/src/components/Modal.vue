f2993ed2fd557b8da335b9e3d468cb3db3faaabe
qwe8851 <dgh07027@naver.com>
2023-01-15 23:17:33 +0900

    사용자 input 받는 방법 (v-modal)
    @input = '.target.value' 까지

--- basic/vuedongsan/src/components/Modal.vue
+++ basic/vuedongsan/src/components/Modal.vue
@@ -5,8 +5,10 @@
             <img :src="원룸들[누른거].image" style="width : 100%">
             <h4>{{원룸들[누른거].title}}</h4>
             <p>{{원룸들[누른거].content}}</p>
-            <p>{{원룸들[누른거].price}} 원</p>
-            <!-- <button @click="모달창열렸니 =false">닫기</button> -->
+            <!-- <input @input="month = $event.target.value"> 와 동일 -->
+            <input v-model="month" type="number" style="width: 30px">개월 할부 
+            <p>{{원룸들[누른거].price / month}} 원</p>
+            <button @click="$emit('closeModal')">닫기</button>
         </div>
     </div>
 </template>
@@ -15,6 +17,11 @@
 export default {
     //eslint-disable-next-line
     name : 'Modal',
+    data() {
+        return {
+            month : 1,
+        }
+    },
     props : {
         원룸들 : Array,
         누른거 : Number,
