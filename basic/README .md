0b81f4ef9001f11eb843d9cf9a2dc5c6d679dd9a
qwe8851 <dgh07027@naver.com>
2023-01-15 21:12:08 +0900

    HTML을 단순화 할 수 이쓴 Compoent 생성

--- basic/README .md
+++ basic/README .md
@@ -5,3 +5,8 @@
 
 ### 실행
 `npm run serve`
+
+<br><br><br>
+---
+### component 생성 시 컴포넌트 네임 에러 주석  
+//eslint-disable-next-line
--- basic/vuedongsan/src/App.vue
+++ basic/vuedongsan/src/App.vue
@@ -4,7 +4,7 @@
   <div class="black-bg" v-if="모달창열렸니 == true">
     <div class="white-bg">
       <h4>{{원룸들[누른거].title}}</h4>
-        <img :src="원룸들[누른거].image" class="room-img">
+        <img :src="원룸들[누른거].image" style="width : 100%">
         <h4>{{원룸들[누른거].title}}</h4>
         <p>{{원룸들[누른거].content}}</p>
         <p>{{원룸들[누른거].price}}</p>
@@ -12,13 +12,16 @@
     </div>
   </div>
 
+  <!-- 할인 배너 -->
+  <Discount/>
+
   <div class="menu">
     <a v-for="menu in menus" :key="menu" >{{ menu }}</a>
   </div>
   <div v-for="(item, i) in 원룸들" :key="i">
     <img :src= "item.image" class="room-img"/>
     <h4 @click="모달창열렸니 = true; 누른거 = i;">{{item.title}}</h4>
-    <p>{{item.content}}</p>
+    <!-- <p>{{item.content}}</p> -->
     <p>{{item.price}}원</p>
   </div>
 
@@ -27,6 +30,7 @@
 
 <script>
 import oneroom from "./assets/oneroom.js";
+import Discount from "./Discount.vue";
 
 export default {
   name: 'App',
@@ -44,6 +48,7 @@ export default {
     }
   },
   components: {
+    Discount : Discount,  // es6 : 왼쪽오른쪽 항목이 같으므로 그냥 "Discount"만 사용해도 동작
   }
 }
 </script>
@@ -56,6 +61,12 @@ div{
   box-sizing: border-box;
 }
 
+.discount{
+  background: #eee;
+  padding: 10px;
+  margin: 10px;
+  border-radius: 5px;
+}
 .black-bg{
   width: 100%; height: 100%;
   background: rgba(0,0,0,0.5);
@@ -68,13 +79,10 @@ div{
   border-radius: 8px;
   padding: 20px;
 }
-
-
 .room-img{
   width: 100%;
   margin-top: 40px;
 }
-
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -82,13 +90,11 @@ div{
   text-align: center;
   color: #2c3e50;
 }
-
 .menu{
   background: darkslateblue;
   padding: 15px;
   border-radius: 5px;
 }
-
 .menu a{
   color: white;
   padding: 10px;
