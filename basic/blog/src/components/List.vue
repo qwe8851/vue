19493ef525787a95ea35b1bfe3e5557f6190f85f
qwe8851 <dgh07027@naver.com>
2023-01-21 00:39:19 +0900

    블로그 글목록 만들기 & 데이터 바인딩, props 복습

---
+++ basic/blog/src/components/List.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h5>{{블로그.title}}</h5>
+        <p>{{블로그.date}}</p>
+    </div>
+</template>
+
+<script>
+
+export default {
+    //eslint-disable-next-line
+    name: 'List',
+    data(){
+        return{
+
+        }
+    },
+    props: {
+        블로그 :  Object,
+    },
+
+}
+</script>
+
+<style>
+
+</style>
